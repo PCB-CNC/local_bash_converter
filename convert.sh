536f477aa28c53a5330f618dc17c83d8c72bdcd4
GuilhermeBraz <gvcerveirabraz@gmail.com>
2023-02-10 11:15:47 -0300

    zipar gcode para gcode.zip

--- convert.sh
+++ convert.sh
@@ -5,3 +5,8 @@
 ./bin/gbr2ngc gerber/Gerber_TopLayer.GTL --radius 0.0025 --output gcode/Top.gcode
 ./bin/gbr2ngc gerber/Gerber_BoardOutlineLayer.GKO --radius 0.0025 --output gcode/Outline.gcode
 
+# zip the gcode files , only the files not the folder
+zip gcode.zip gcode/*
+
+#copy the zip file to the downloads folder
+cp gcode.zip ~/Downloads/
